6772d7b5f5fa34b88daa02ccaf1d8331ee4898b3
jovanicmilica <jovanic.sv9.2023@uns.ac.rs>
2025-12-17 11:27:23 +0100

    Replace href with routerLink in navbar
    - Update all navigation links in the navbar from using <a href="..."> to Angular's routerLink directive
    - Fix issues with logo and links not navigating correctly

--- web/taxi-app-web/src/app/home/home.ts
+++ web/taxi-app-web/src/app/home/home.ts
@@ -1,10 +1,11 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { NavbarComponent } from '../shared/navbar/navbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RouterModule],
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
 })
