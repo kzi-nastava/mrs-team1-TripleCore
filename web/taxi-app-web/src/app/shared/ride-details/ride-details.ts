bfff70f1162df41fc7c76eae51962a9e11952b4d
dkomatovic <danicakomatovic@gmail.com>
2025-12-22 16:48:52 +0100

    Create ride-details component

---
+++ web/taxi-app-web/src/app/shared/ride-details/ride-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ride-details',
+  imports: [],
+  templateUrl: './ride-details.html',
+  styleUrl: './ride-details.css',
+})
+export class RideDetails {
+
+}
