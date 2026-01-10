447597d02f3e866b1e98883ef541c0a991c4b3cb
dkomatovic <danicakomatovic@gmail.com>
2026-01-10 22:38:37 +0100

    Create DriverRepository and VehicleRepository

---
+++ backend/Projekatsiit2023/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/repository/VehicleRepository.java
@@ -0,0 +1,8 @@
+package rs.ac.uns.ftn.asd.Projekatsiit2023.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rs.ac.uns.ftn.asd.Projekatsiit2023.models.Vehicle;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+}
