ae7e52517f3e5f7121af64cc5994728aec5d0966
dkomatovic <danicakomatovic@gmail.com>
2026-01-12 22:56:52 +0100

    Create Admin class, repository and service

---
+++ backend/Projekatsiit2023/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package rs.ac.uns.ftn.asd.Projekatsiit2023.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rs.ac.uns.ftn.asd.Projekatsiit2023.models.Admin;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+}
