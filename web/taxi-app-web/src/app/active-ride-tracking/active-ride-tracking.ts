19bf79ea9276873ccbd1a30a0879f6c06f63b0f4
dkomatovic <danicakomatovic@gmail.com>
2026-01-23 01:05:29 +0100

    Create active ride tracking component

---
+++ web/taxi-app-web/src/app/active-ride-tracking/active-ride-tracking.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-active-ride-tracking',
+  imports: [],
+  templateUrl: './active-ride-tracking.html',
+  styleUrl: './active-ride-tracking.css',
+})
+export class ActiveRideTrackingComponent {
+
+}
