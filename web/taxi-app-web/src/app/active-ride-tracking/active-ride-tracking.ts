c0e29a30ffcd61f8f27f4ff762e1fba1b4cd42a3
dkomatovic <danicakomatovic@gmail.com>
2026-01-23 19:01:59 +0100

    Create and style active ride tracking component

--- web/taxi-app-web/src/app/active-ride-tracking/active-ride-tracking.ts
+++ web/taxi-app-web/src/app/active-ride-tracking/active-ride-tracking.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MapComponent } from '../map/map';
+import { RideDetailsResponse } from '../models/ride-details-response';
+import { MOCK_RIDE_DETAILS } from './mock-ride';
 
 @Component({
   selector: 'app-active-ride-tracking',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    CommonModule,
+    MapComponent
+  ],
   templateUrl: './active-ride-tracking.html',
   styleUrl: './active-ride-tracking.css',
 })
 export class ActiveRideTrackingComponent {
+  @Output() close = new EventEmitter<void>();
+
+  closeSelf(): void {
+    this.close.emit();
+  }
+
+  @Input() ride: RideDetailsResponse = MOCK_RIDE_DETAILS;
+  
 
 }
--- web/taxi-app-web/src/app/app.config.ts
+++ web/taxi-app-web/src/app/app.config.ts
@@ -107,6 +107,14 @@ const routes: Routes = [
     loadComponent: () => import('./driver/driver-reviews/driver-reviews').then(m => m.DriverReviewsComponent)
   },
   {
+    path: 'active-ride-tracking',
+    loadComponent: () => import('./active-ride-tracking/active-ride-tracking').then(m => m.ActiveRideTrackingComponent)
+  },
+  {
+    path: 'test',
+    loadComponent: () => import('./test/test').then(m => m.TestComponent)
+  },
+  {
     path: '**',
     redirectTo: ''
   }
