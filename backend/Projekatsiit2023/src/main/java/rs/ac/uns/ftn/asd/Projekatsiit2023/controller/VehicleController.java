55f923476b7ed0d919e1f150e8812ca67905aa35
dkomatovic <danicakomatovic@gmail.com>
2026-01-26 18:15:57 +0100

    Implement moving an active vehicle along its route

--- backend/Projekatsiit2023/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/controller/VehicleController.java
+++ backend/Projekatsiit2023/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2023/controller/VehicleController.java
@@ -2,17 +2,19 @@ package rs.ac.uns.ftn.asd.Projekatsiit2023.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rs.ac.uns.ftn.asd.Projekatsiit2023.dto.response.vehicle.ActiveRideVehicleDetailsResponse;
 import rs.ac.uns.ftn.asd.Projekatsiit2023.dto.response.vehicle.ActiveVehicleLocationResponse;
 import rs.ac.uns.ftn.asd.Projekatsiit2023.models.ActiveVehicle;
+import rs.ac.uns.ftn.asd.Projekatsiit2023.models.Location;
 import rs.ac.uns.ftn.asd.Projekatsiit2023.models.Ride;
+import rs.ac.uns.ftn.asd.Projekatsiit2023.models.Route;
 import rs.ac.uns.ftn.asd.Projekatsiit2023.services.RideService;
+import rs.ac.uns.ftn.asd.Projekatsiit2023.services.RouteService;
 import rs.ac.uns.ftn.asd.Projekatsiit2023.services.VehicleService;
+import rs.ac.uns.ftn.asd.Projekatsiit2023.services.impl.RouteServiceImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,12 +23,15 @@ public class VehicleController {
 
     private final VehicleService vehicleService;
     private final RideService rideService;
+    private final RouteServiceImpl routeService;
 
     public VehicleController(
             VehicleService vehicleService,
-            RideService rideService){
+            RideService rideService,
+            RouteServiceImpl routeService){
         this.vehicleService = vehicleService;
         this.rideService = rideService;
+        this.routeService = routeService;
     }
 
     @GetMapping("/locations")
@@ -53,6 +58,34 @@ public class VehicleController {
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
+    }
+
+    @PostMapping("/active-vehicle/{id}/test-route-service")
+    public ResponseEntity<?> testRouteService(@PathVariable("id") Long id){
+        try{
+            ActiveVehicle av = vehicleService.getActiveVehicle(id);
+            List<Location> points = new ArrayList<>();
+            points.add(av.getLocation());
+
+            Location herojaPinkija16 = new Location(
+                    45.2459,
+                    19.8425,
+                    "Heroja Pinkija 16, Novi Sad"
+            );
+
+            points.add(herojaPinkija16);
+
+            vehicleService.setRouteForActiveVehicle(id, points);
+            return ResponseEntity.ok("Route added to active vehicle");
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/active-vehicle/{id}/move")
+    public ResponseEntity<?> moveActiveVehicle(@PathVariable("id") Long id){
+        vehicleService.moveActiveVehicle(id);
+        return ResponseEntity.ok("Vehicle moved");
     }
 
 }
