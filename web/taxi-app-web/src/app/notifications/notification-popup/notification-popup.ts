ae35025f4284f405f301c488fba36a37e8e2d45a
dkomatovic <danicakomatovic@gmail.com>
2026-02-04 21:11:34 +0100

    Create notifications page and notification popup components

---
+++ web/taxi-app-web/src/app/notifications/notification-popup/notification-popup.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-popup',
+  imports: [],
+  templateUrl: './notification-popup.html',
+  styleUrl: './notification-popup.css',
+})
+export class NotificationPopupComponent {
+
+}
---
+++ web/taxi-app-web/src/app/notifications/notifications-page/notifications-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-page',
+  imports: [],
+  templateUrl: './notifications-page.html',
+  styleUrl: './notifications-page.css',
+})
+export class NotificationsPageComponent {
+
+}
