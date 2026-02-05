34ee7aeafccd3164a9d93d60d194d5af5c628ae6
dkomatovic <danicakomatovic@gmail.com>
2026-02-05 20:02:20 +0100

    Add images to the notification cards

--- web/taxi-app-web/src/app/notifications/notification-popup/notification-popup.ts
+++ web/taxi-app-web/src/app/notifications/notification-popup/notification-popup.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NotificationResponse } from '../../models/notification-response';
+import { MatCard } from '@angular/material/card';
+import { MatCardContent } from '@angular/material/card';
+import { ChangeDetectorRef } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-notification-popup',
-  imports: [],
+  standalone: true,
+  imports: [MatCard, MatCardContent, RouterLink],
   templateUrl: './notification-popup.html',
   styleUrl: './notification-popup.css',
 })
-export class NotificationPopupComponent {
+export class NotificationPopupComponent implements OnInit{
+  @Input() notification!: NotificationResponse;
+  link: any[] = [];
+
+  constructor (private cdr: ChangeDetectorRef){}
+
+  @Output() close = new EventEmitter<void>();
+
+  closeSelf(): void {
+    this.close.emit();
+  }
+
+  ngOnInit(): void {
+    if (this.notification.link) {
+      const parts = this.notification.link.split(':');
+      if (parts[0] == 'ride-tracking'){
+        this.link = ['/active-ride-tracking', parts[1]];
+      } else if (parts[0] == 'review'){
+        this.link = ['passenger/passenger-my-rides']
+      }
+    }
+    this.cdr.detectChanges();
+  }
 
 }
--- web/taxi-app-web/src/app/notifications/notifications-page/notifications-page.ts
+++ web/taxi-app-web/src/app/notifications/notifications-page/notifications-page.ts
@@ -1,22 +1,26 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotificationResponse } from '../../models/notification-response';
+import { NotificationPopupComponent } from '../notification-popup/notification-popup';
 
 @Component({
   selector: 'app-notifications-page',
-  imports: [ CommonModule ],
+  imports: [ CommonModule, NotificationPopupComponent ],
   standalone: true,
   templateUrl: './notifications-page.html',
   styleUrl: './notifications-page.css',
 })
 export class NotificationsPageComponent {
-  notifications: NotificationResponse[] = [
+  isChildOpen = false;
+  selectedNotification!: NotificationResponse;
+
+  notifications: any[] = [
   {
     id: 1,
     recipientId: 10,
     title: 'Vožnja završena',
     message: 'Vaša vožnja je uspešno završena. Ostavite ocenu za vozača.',
-    link: 'make-review:101',
+    link: 'review:101',
     time: '2026-02-03T18:45:00',
     seen: false
   },
@@ -25,7 +29,7 @@ export class NotificationsPageComponent {
     recipientId: 10,
     title: 'Aktivna vožnja',
     message: 'Vaša vožnja je u toku. Možete pratiti kretanje vozila uživo.',
-    link: 'track-ride:202',
+    link: 'ride-tracking:202',
     time: '2026-02-03T17:30:00',
     seen: true
   },
@@ -34,7 +38,7 @@ export class NotificationsPageComponent {
     recipientId: 10,
     title: 'Podsetnik',
     message: 'Imate nepročitanu poruku vezanu za vašu rezervaciju.',
-    link: 'track-ride:203',
+    link: 'ride-tracking:203',
     time: '2026-02-02T09:15:00',
     seen: false
   },
@@ -43,10 +47,19 @@ export class NotificationsPageComponent {
     recipientId: 10,
     title: 'Hvala što koristite aplikaciju',
     message: 'Vaša povratna informacija nam pomaže da unapredimo uslugu.',
-    link: 'make-review:104',
+    link: 'review:104',
     time: '2026-02-01T21:05:00',
     seen: true
   }
 ];
 
+
+  openChild(notification: NotificationResponse): void {
+    this.selectedNotification = notification;
+    this.isChildOpen = true;
+  }
+
+  closeChild(): void {
+    this.isChildOpen = false;
+  }
 }
